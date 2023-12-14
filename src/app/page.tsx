484e7907b1f45a8f746fba80d1cfd4c992ce1318
Francisco Collazos <fcollazos@internetbrands.com>
2023-12-14 08:16:36 -0500

    adjust style of navigation button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header>
+      <header className="flex justify-between items-center mb-4">
         <h1 className={"text-2xl"}>TODOS</h1>
-        <Link href="/testpage">New page</Link>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 outline-none"
+          href="/testpage"
+        >
+          New page
+        </Link>
       </header>
       <ul></ul>
     </>
